1ad2d90ba2c8a6942a0cb0c0aac280b93e83e7d3
ThomasLaforge <thomas.laforge.38@gmail.com>
2023-09-06 08:43:28 +0200

    TP-express

--- src/index.ts
+++ src/index.ts
@@ -78,3 +78,39 @@ console.log(data) // => JP
 import "dotenv/config"
 
 console.log("env", process.env.NAME)
+
+import express from 'express'
+import path from "path"
+import { rootCertificates } from "tls"
+const app = express()
+const port = 8878
+
+app.use(express.static('public'))
+
+app.get('/toto', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/tata', (req, res) => {
+  res.send(JSON.stringify(p2))
+})
+
+app.get('/random-between/:min/:max', (req, res) => {
+  const min = parseInt(req.params.min)
+  const max = parseInt(req.params.max)
+  const random = Math.floor(Math.random() * (max - min + 1)) + min
+  res.send(random.toString())
+})
+
+// Create a route who return the image on /public/image.jpg
+app.get('/simplon/logo', (req, res) => {
+  res.sendFile('logo.png', { root: path.resolve(__dirname, "../public")})
+})
+
+app.get('/simplon/web', (req, res) => {
+  res.sendFile('index.html', { root: path.resolve(__dirname, "../public")})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
